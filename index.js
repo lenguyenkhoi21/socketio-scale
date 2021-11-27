7c33d46521ee17d8deda2cf918634d5433bbffa7
lenguyenkhoi21 <lenguyenkhoi21@gmail.com>
2021-11-27 09:08:33 +0700

    Done <3 :)))))

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const { createServer } = require("http")
 const { Server } = require("socket.io")
 const { createClient } = require('redis')
 const { createAdapter } = require("@socket.io/redis-adapter")
-const app = express()
+const { Emitter } = require("@socket.io/redis-emitter")
+const {request} = require('express')
+
 const SERVER_NAME = process.env.SERVER_NAME || 'Unknown'
 const PORT = process.env.PORT || 3000
 const HOST_REDIS = process.env.HOST_REDIS || '192.168.1.11'
 const PORT_REDIS = process.env.PORT_REDIS || 6371
 
 
+const app = express()
 const httpServer = createServer(app)
 
 const io = new Server(httpServer, {
     cors: {
-        origin: ['http://localhost:4000', 'http://127.0.0.1:4000', 'http://192.168.1.11:4000'],
+        origin: ['http://localhost:4000', 'http://127.0.0.1:4000', 'http://192.168.1.4:4000'],
         methods: ['GET', 'POST'],
         credentials: true
     }
@@ -22,42 +25,31 @@ const io = new Server(httpServer, {
 
 const pubClient = createClient({ host: HOST_REDIS, port: PORT_REDIS })
 const subClient = pubClient.duplicate()
+//const emitter = new Emitter(pubClient)
 
 io.adapter(createAdapter(pubClient, subClient))
 
 const conn = new Map()
 
-io.on('connection', (socket) => {
+io.use((socket, next) => {
+    console.log(`${SERVER_NAME} --- middleware --- ${socket.id}`)
+    const user = socket.request.headers.username
+    if (user === undefined || user === null) {
+        next(new Error(`${SERVER_NAME} --- No user regis`))
+    }
+    console.log(`${SERVER_NAME} --- Welcome user --- ${user}`)
     conn.set(socket.id, socket)
-    console.log(`${SERVER_NAME} --- ${socket.id}`)
+    conn.get(socket.id)[`username`] = user.name
+    socket.join(`${socket.request.headers.username}`)
+    next()
+})
 
-    socket.on('register-user', user => {
-        conn.get(socket.id)[`username`] = user.name
-        console.log(`${SERVER_NAME} --- Welcome ${user.name}  ! --- The total of user in server: ${conn.size}`)
-    })
+io.on('connection', (socket) => {
+    console.log(`${SERVER_NAME} --- ${socket.id}`)
 
     socket.on('private-message', msg => {
         console.log(`${SERVER_NAME} has been received, now notify to other node !`)
-        socket.broadcast.emit('handle-message', msg)
-        //console.log(`${SERVER_NAME} --- message send: ${msg.message}`)
-        // conn.forEach(client => {
-        //     console.log(`${SERVER_NAME} --- client id : ${client.id} --- username: ${client.username}`)
-        //     if (client.username === msg.receiver) {
-        //         emitter.emit('receiver-message', msg.message)
-        //         console.log(`${SERVER_NAME} --- Send message from ${client.username} to ${msg.receiver} with message: ${msg.message} !`)
-        //     }
-        // })
-    })
-
-    socket.on('handle-message', msg => {
-        console.log(`${SERVER_NAME} --- receiver : ${msg.receiver} --- message : ${msg.message}`)
-        conn.forEach(client => {
-            console.log(`${SERVER_NAME} --- client id : ${client.id} --- username: ${client.username}`)
-            if (client.username === msg.receiver) {
-                socket.broadcast.emit('receiver-message', msg.message)
-                console.log(`${SERVER_NAME} --- Send message from ${client.username} to ${msg.receiver} with message: ${msg.message} !`)
-            }
-        })
+        io.to(`${msg.receiver}`).emit('receiver-message', msg.message)
     })
 
     socket.on('disconnect', () => {
